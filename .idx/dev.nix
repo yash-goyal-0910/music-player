98bc50b7049ea9ca36c4ef17b1dbf2c00c8da1d3
yashgoyal09102005 <yashgoyal09102005@gmail.com>
2024-09-25 06:22:51 +0000

    added required pakages

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,14 +6,13 @@
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
-    # pkgs.python311Packages.pip
-    # pkgs.sudo
-    # pkgs.doas-sudo-shim
+    pkgs.go
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.nodejs_20
+    pkgs.nodePackages.nodemon
+    pkgs.python311Packages.pip
+    pkgs.python3
   ];
 
   # Sets environment variables in the workspace
